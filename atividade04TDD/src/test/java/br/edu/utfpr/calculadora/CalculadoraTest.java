73fbbe30587800acf1ed0bf813cd1f36d6b72705
Rafael Santos <rafaelnsantos1@gmail.com>
2018-08-17 00:13:31 -0300

    testes dba

--- atividade04TDD/src/test/java/br/edu/utfpr/calculadora/CalculadoraTest.java
+++ atividade04TDD/src/test/java/br/edu/utfpr/calculadora/CalculadoraTest.java
@@ -1,24 +1,44 @@
 package br.edu.utfpr.calculadora;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculadoraTest {
 
+    Calculadora c;
+    Funcionario f;
+
+    public CalculadoraTest(){
+        c = new Calculadora();
+    }
+
     @Test
     public void salarioDesenvolvedorMaior3000(){
-        Calculadora c = new Calculadora();
-        Funcionario f = new Funcionario(Cargo.DESENVOLVEDOR, 5000f);
+        f = new Funcionario(Cargo.DESENVOLVEDOR, 5000f);
 
         assertEquals(4000f, c.calcularSalarioLiquido(f), 0.1f);
     }
 
     @Test
     public void salarioDesenvolvedorMenor3000(){
-        Calculadora c = new Calculadora();
-        Funcionario f = new Funcionario(Cargo.DESENVOLVEDOR, 1000f);
+        f = new Funcionario(Cargo.DESENVOLVEDOR, 1000f);
 
         assertEquals(900f, c.calcularSalarioLiquido(f), 0.1f);
+    }
+
+    @Test
+    public void salarioDBAMaior2000(){
+        f = new Funcionario(Cargo.DBA, 1000f);
+
+        assertEquals(750f, c.calcularSalarioLiquido(f), 0.1f);
+    }
+
+    @Test
+    public void salarioDBAMenor2000(){
+        f = new Funcionario(Cargo.DBA, 10000f);
+
+        assertEquals(8500f, c.calcularSalarioLiquido(f), 0.1f);
     }
 }
