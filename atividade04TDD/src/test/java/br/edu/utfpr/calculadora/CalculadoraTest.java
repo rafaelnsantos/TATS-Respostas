d67c939d7277904eab5614f16d5ed0cfbfa609fc
Rafael Santos <rafaelnsantos1@gmail.com>
2018-08-17 00:09:18 -0300

    add teste desenvolvedor salario menor 3000

--- atividade04TDD/src/test/java/br/edu/utfpr/calculadora/CalculadoraTest.java
+++ atividade04TDD/src/test/java/br/edu/utfpr/calculadora/CalculadoraTest.java
@@ -7,10 +7,18 @@ import static org.junit.Assert.*;
 public class CalculadoraTest {
 
     @Test
-    public void salarioDesenvolvedor5000(){
-        Caclculadora c = new Calculadora();
+    public void salarioDesenvolvedorMaior3000(){
+        Calculadora c = new Calculadora();
         Funcionario f = new Funcionario(Cargo.DESENVOLVEDOR, 5000f);
 
         assertEquals(4000f, c.calcularSalarioLiquido(f), 0.1f);
+    }
+
+    @Test
+    public void salarioDesenvolvedorMenor3000(){
+        Calculadora c = new Calculadora();
+        Funcionario f = new Funcionario(Cargo.DESENVOLVEDOR, 1000f);
+
+        assertEquals(900f, c.calcularSalarioLiquido(f), 0.1f);
     }
 }
